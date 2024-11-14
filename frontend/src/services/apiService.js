79a57cac3ea617fb143b17af7c49c9c8ea5a0661
Dakshya Chauhan <155088459+Dakshya52@users.noreply.github.com>
2024-11-14 20:33:10 +0530

    Update apiService.js

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000', // Change to your backend URL
+  baseURL: 'https://aidataextractionbackend.netlify.app/', // Change to your backend URL
 });
 
 export default instance;
